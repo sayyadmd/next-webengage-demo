d57f39e7bc0f258f9c4f966f2707d3ae4fd1d215
Sayyad Mahammad <sayyadmahammad@aesl.in>
2025-10-13 12:28:04 +0530

    added dynamic userId

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
-import Link from "next/link";
 import { sendGTMEvent } from "@next/third-parties/google";
+import React, { useState } from "react";
 import toast from "react-hot-toast";
 
 const notify = (event) => toast.success(event);
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState("");
+  const [finalValue, setFinalValue] = useState(null);
+
+  const handleClick = () => {
+    setFinalValue(Number(inputValue));
+    console.log("#####userId", inputValue);
+  };
   function sendEvent(event) {
     let gtmProps = {
       event: "tap_gtmButton",
@@ -12,20 +19,19 @@ export default function Home() {
         value: "xyz_abc",
         psid: 115500,
         email: "johnvick@example.com",
-        user_id: "john_vick4",
+        userId: finalValue,
       },
     };
     switch (event) {
       case "login":
         gtmProps = {
-          event: "tap_login",
+          event: "tap_login_login",
           eventParams: {
             value: "xyz_abc",
             psid: 115500,
             email: "johnvick@example.com",
-            user_id: "john_vick4",
           },
-          login_id: "12341234",
+          userId: finalValue,
         };
         break;
       case "home":
@@ -35,7 +41,6 @@ export default function Home() {
             value: "xyz_abc",
             psid: 115500,
             email: "johnvick@example.com",
-            user_id: "john_vick4",
           },
         };
         break;
@@ -46,7 +51,6 @@ export default function Home() {
             value: "xyz_abc",
             psid: 115500,
             email: "johnvick@example.com",
-            user_id: "john_vick4",
           },
         };
         break;
@@ -57,7 +61,6 @@ export default function Home() {
             value: "xyz_abc",
             psid: 115500,
             email: "johnvick@example.com",
-            user_id: "john_vick4",
           },
         };
         break;
@@ -68,9 +71,7 @@ export default function Home() {
             value: "xyz_abc",
             psid: 115500,
             email: "johnvick@example.com",
-            user_id: "john_vick4",
           },
-          login_id: "12341234",
         };
         break;
       default:
@@ -81,12 +82,21 @@ export default function Home() {
   }
   return (
     <main>
-      <p>
-        <Link href="/about">Go to About</Link>
-      </p>
-      <p>
-        <Link href="/posts/1">View post 1 (dynamic)</Link>
-      </p>
+      <h1>GTM-Webengage Demo App</h1>
+      <input
+        type="number"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Enter a number"
+        className="border p-2 rounded w-40"
+      />
+
+      <button
+        onClick={handleClick}
+        className="bg-blue-600 text-white px-4 py-2 rounded"
+      >
+        Set Value
+      </button>
       <div className="flex flex-col w-[20%]">
         <button
           className="m-5 p-4 bg-blue-500 text-white rounded cursor-pointer"
